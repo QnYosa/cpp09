0c987a1deb6a3a728623faac9271761a057b6641
QnYosa <dimitriyoula@gmail.com>
2023-03-24 20:06:02 +0100

    exo 2 ongoing

---
+++ ex00/BitcoinExchange.cpp
@@ -0,0 +1,20 @@
+#include <map>
+// map ? int, int 
+
+
+int	algo(std::map <int, int> &map, int value)
+{
+	map.find(.)
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	// test if file is a file.
+	//check each line of the file to see if it respect the format 
+
+	// fill the map and check all the keys.
+	// must mutilply the value by the conversion rate.
+	// transform date format to android date format 2012-03-13 = 20120313
+
+}
